9e873529f2e4bd60651fb1ba7c91662765d14726
Gibson Gelera <76902828+gibsongelera@users.noreply.github.com>
2024-03-14 22:42:37 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,27 +1,24 @@
-window.onload = function() {
-    $(document).ready(function() {
-      var envelope = $('#envelope');
-      var btn_open = $("#open");
-      var btn_reset = $("#reset");
+$(document).ready(function() {
+    var envelope = $('#envelope');
+    var btn_open = $("#open");
+    var btn_reset = $("#reset");
   
-      envelope.click(function() {
-        open();
-      });
-      btn_open.click(function() {
-        open();
-      });
-      btn_reset.click(function() {
-        close();
-      });
-  
-      function open() {
-        envelope.addClass("open")
-          .removeClass("close");
-      }
-      function close() {
-        envelope.addClass("close")
-          .removeClass("open");
-      }
+    envelope.click(function() {
+      open();
     });
-  }
+    btn_open.click(function() {
+      open();
+    });
+    btn_reset.click(function() {
+      close();
+    });
   
+    function open() {
+      envelope.addClass("open")
+        .removeClass("close");
+    }
+    function close() {
+      envelope.addClass("close")
+        .removeClass("open");
+    }
+  });
